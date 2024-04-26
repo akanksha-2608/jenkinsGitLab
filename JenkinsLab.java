b6f99285e0ffd3189f211353623c45b24e149e37
akanksha-2608 <166794746+akanksha-2608@users.noreply.github.com>
2024-04-26 12:31:08 +0530

    Update JenkinsLab.java

--- JenkinsLab.java
+++ JenkinsLab.java
@@ -1,6 +1,6 @@
 public class JenkinsLab{
 
         public static void main(String[] args){
-                System.out.println("Author:Rajan -Github Jenkins Build Demo");
+                System.out.println("Author:Akanksha -Github Jenkins Build Demo");
         }
 }
